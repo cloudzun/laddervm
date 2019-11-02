d5d2a21da9e937adf6d7aca70dbb13cc1c42c853
CloudZun <info@cloudzun.com>
2019-11-02 22:03:57 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,3 +1,3 @@
 az group create --name Ladder --location southeastasia
-az vm create --resource-group Ladder --name myLadder01 --image UbuntuLTS --admin-username chengzh --generate-ssh-keys --size  Standard_B1ls --custom-data SS.txt  
+az vm create --resource-group Ladder --name myLadder01 --image UbuntuLTS --admin-username chengzh --generate-ssh-keys --size  Standard_B1s --custom-data SS.txt  
 az vm open-port --resource-group Ladder --name myLadder01 --port 443 --priority 1001
